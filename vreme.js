48d5eb9f83f1ba3fe958a87d96ac0455900fe149
masterML5 <milosjelic98@gmail.com>
2022-06-09 21:54:00 +0200

    cookie vreme

--- vreme.js
+++ vreme.js
@@ -6,8 +6,9 @@ let paracin = document.getElementById('vreme_par');
 let prokuplje = document.getElementById('vreme_pro');
 
 let svi_gradovi = ['subotica', 'beograd', 'paracin', 'prokuplje'];
+let gradovilen = svi_gradovi.length;
 
-console.log(svi_gradovi);
+
 
 
 
@@ -49,30 +50,67 @@ function toggle(source) {
 }
 
 }
+var value = 'vreme'
+var match = document.cookie.match(new RegExp('(^| )' + value + '=([^;]+)'));
 
-const kuki = document.cookie.slice(6);
+const kuki = match[0].slice(6);
 let imena = kuki.split("-");
 imena = imena.filter(Boolean);
 let brojvremena = imena.length;
 if(imena[0] == 'sve'){
     brojvremena = 4;
 }
+
 let vremenska = document.getElementById('vremenska_baner');
 vremenska.style.display = "grid";
 vremenska.style.gridTemplateColumns = `repeat(${brojvremena}, 3fr)`;
 
+
+let difference = svi_gradovi.filter(x => !imena.includes(x));
+let diflen = difference.length;
+
+
+
+
+
+
 let ptags = document.getElementsByTagName('p');
-ptagslen = ptags.length;
+const ptagslen = ptags.length;
+
+if(imena[0] == 'sve'){
+    for(let i = 0; i < gradovilen; i++){
+        for(let j = 0; j < ptagslen; j++){
+            if(imena[i] == ptags[j].id.toLowerCase()){
+                ptags[j].style.display = 'block';
+            }
+        }
+    }
+}else{
+
+
+
+
+
 for(let l = 0; l < brojvremena; l++){
-    for(let m = 0; m < ptagslen; m++)
-   if(imena[l] == ptags[m].innerText.toLowerCase()){
+    for(let m = 0; m < ptagslen; m++){
+ 
+   if(imena[l] == ptags[m].id.toLowerCase()){
+     
         ptags[m].style.display = 'block';
-       
-   }else if(imena )
+   }
+}
 }
 
+for(let i = 0; i < diflen; i++){
+    for(let j = 0; j < ptagslen; j++){
 
+        if(difference[i] == ptags[j].id.toLowerCase()){
+            ptags[j].style.display = 'none';
+        }
+    }
+}
 
+}
 
 
 
